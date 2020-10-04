174f7c6d9fd62cd5ad975aa3f0073c26c1138503
DevOpsAT_Admin <F5-g6-h7>
2020-10-04 12:54:27 +0300

    create DZ4_2

---
+++ src/main/java/ru/zotov/utils/dz4/CandyBox/BoxCandy.java
@@ -0,0 +1,66 @@
+package ru.zotov.utils.dz4.CandyBox;
+
+import lombok.Getter;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoxCandy implements Box {
+
+    @Getter
+    private double allowWeight;
+
+    @Getter
+    private List<Product> listProduct;
+
+    public BoxCandy(double maxWeight) {
+        this.allowWeight = maxWeight;
+        listProduct = new ArrayList<>();
+    }
+
+    public BoxCandy() {
+        this.allowWeight = 20;
+        listProduct = new ArrayList<>();
+    }
+
+    @Override
+    public boolean put(Product product) {
+        if (product.getWeight() + getSummaryWeight() <= allowWeight) {
+            listProduct.add(product);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Double getSummaryWeight() {
+        double summWeight = 0;
+        for (Product a : listProduct)
+            summWeight += a.getWeight();
+        return summWeight;
+    }
+
+    @Override
+    public Double getFullPrice() {
+        double summPrice = 0;
+        for (Product a : listProduct)
+            summPrice += a.getPrice();
+        return summPrice;
+    }
+
+    @Override
+    public void showAllProducts() {
+        for (Product a : listProduct) {
+            System.out.println(a.getName() + " " + a.getPrice() + " " + a.getWeight() + ";");
+        }
+        System.out.println("All position: " + listProduct.size() + ";");
+        System.out.println("Full price: " + this.getFullPrice() + ";");
+        System.out.println("Summary weight: " + this.getSummaryWeight() + ".");
+    }
+
+    @Override
+    public void clear() {
+        listProduct.clear();
+    }
+}
